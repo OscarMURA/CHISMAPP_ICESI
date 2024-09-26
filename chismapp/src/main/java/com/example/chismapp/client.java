72a92f7c606c265e03f2ead8bf4128b265a890e4
diegopolancolozano <diegoarmandopolancolozano@gmail.com>
2024-09-26 15:36:51 -0500

    added basic structure

---
+++ chismapp/src/main/java/com/example/chismapp/client.java
@@ -0,0 +1,13 @@
+package com.example.chismapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class client {
+
+    public static void main(String[] args) {
+        SpringApplication.run(client.class, args);
+    }
+
+}
---
+++ chismapp/src/test/java/com/example/chismapp/ChismappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chismapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChismappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
